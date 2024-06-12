14ab08dc321b28619a6291d0351cc903acaee9b8
CryroByte <cadencasteel10@gmail.com>
2024-06-12 14:20:32 -0500

    First Commit Setting up Gradle

---
+++ src/main/java/me/caden/asm/gui/Main.java
@@ -0,0 +1,12 @@
+package me.caden.asm.gui;
+
+/**
+ * @author <b><a href=https://github.com/CadenCCC>Caden</a></b>
+ * @since 1.0.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
