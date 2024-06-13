50b2413816e6ccaaf4087efb227be3110a97a7bb
CryroByte <cadencasteel10@gmail.com>
2024-06-13 04:28:12 -0500

    overallChanges

--- src/main/java/me/caden/kuda/core/Launch.java
+++ src/main/java/me/caden/kuda/core/Launch.java
@@ -1,14 +1,12 @@
-package me.caden.asm.gui;
+package me.caden.kuda.core;
 
 /**
  * @author <b><a href=https://github.com/CadenCCC>Caden</a></b>
  * @since 1.0.0
  */
-public class Main {
+public class Launch {
 
     public static void main(String[] args) {
-        // Setting up all the Gui Stuff for the program
-
-
     }
+
 }
